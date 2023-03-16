7a8f0654bce570cc0cf673a984a02495d466b7c0
Ofilio Herdocia Aráuz <Herdocia20@gmail.com>
2023-03-16 13:36:39 -0600

    Frontend Listening for changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  webpack: function(config, options){
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;  
+  }
 }
+
+/*
+webpackDevMiddleware: (config) => {
+  // Solve compiling problem via vagrant
+  config.watchOptions = {
+    poll: 1000,   // Check for changes every second
+    aggregateTimeout: 300,   // delay before rebuilding
+  };
+  return config;
+}
+*/
 
 module.exports = nextConfig
